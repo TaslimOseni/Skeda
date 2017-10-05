b4e077a2d5f6aeec0b2de9a81ae2702e9a238034
Taslim <tasleemoseni@gmail.com>
2017-10-05 21:06:20 +0100

    inner UI

--- app/src/main/java/com/dabinu/apps/skeda/Inner.java
+++ app/src/main/java/com/dabinu/apps/skeda/Inner.java
@@ -3,12 +3,17 @@ package com.dabinu.apps.skeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Inner extends AppCompatActivity{
 
 
-    TextView head, turner;
+    TextView head, turner, stateText;
+    Spinner hour, minute, ampm;
+    ArrayAdapter<CharSequence> hourAdapt, minAdapt, ampmAdapt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,26 @@ public class Inner extends AppCompatActivity{
 
         head = (TextView) findViewById(R.id.nameOfCarrierIntent);
         turner = (TextView) findViewById(R.id.turner);
+        stateText = (TextView) findViewById(R.id.stateText);
+        hour = (Spinner) findViewById(R.id.hourSpin);
+        minute = (Spinner) findViewById(R.id.minSpin);
+        ampm = (Spinner) findViewById(R.id.ampmSpin);
 
 
         head.setText(getIntent().getStringExtra("NAME"));
         turner.setText(R.string.turnOnString);
+        stateText.setText(R.string.onString);
+
+        hourAdapt = ArrayAdapter.createFromResource(this, R.array.hour, R.layout.timespinner);
+        minAdapt = ArrayAdapter.createFromResource(this, R.array.minute, R.layout.timespinner);
+        ampmAdapt = ArrayAdapter.createFromResource(this, R.array.apm, R.layout.timespinner);
+        hour.setAdapter(hourAdapt);
+        minute.setAdapter(minAdapt);
+        ampm.setAdapter(ampmAdapt);
+
+
+
+
+
     }
 }
