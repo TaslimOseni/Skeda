21402c9813e78909642299e34dd239006bd115e1
Taslim <tasleemoseni@gmail.com>
2017-10-06 17:26:23 +0100

    TGIF2

--- app/src/main/java/com/dabinu/apps/skeda/Child.java
+++ app/src/main/java/com/dabinu/apps/skeda/Child.java
@@ -5,19 +5,19 @@ class Child {
     private String Name;
     private int Image;
 
-    public String getName() {
+    public String getName(){
         return Name;
     }
 
-    public void setName(String Name) {
+    public void setName(String Name){
         this.Name = Name;
     }
 
-    public int getImage() {
+    int getImage(){
         return Image;
     }
 
-    public void setImage(int Image) {
+    void setImage(int Image){
         this.Image = Image;
     }
 }
--- app/src/main/java/com/dabinu/apps/skeda/TimePicker.java
+++ app/src/main/java/com/dabinu/apps/skeda/TimePicker.java
@@ -9,7 +9,6 @@ import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Calendar;
 
 
